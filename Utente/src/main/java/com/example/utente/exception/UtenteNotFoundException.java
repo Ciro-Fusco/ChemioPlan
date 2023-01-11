bcae0df2f22cbd814492a259b0f36ab635687566
ChrisLoc2001 <114596481+ChrisLoc2001@users.noreply.github.com>
2023-01-11 11:09:05 +0100

    Delete Utente directory

--- Utente/src/main/java/com/example/utente/exception/UtenteNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.utente.exception;
-
-public class UtenteNotFoundException extends RuntimeException{
-    public UtenteNotFoundException() {
-    }
-    public UtenteNotFoundException(String message) {
-        super(message);
-    }
-}
--- Utente/src/main/java/com/example/utente/model/Credenziali.java
+++
@@ -1,8 +0,0 @@
-package com.example.utente.model;
-
-import lombok.Data;
-@Data
-public class Credenziali {
-    private String user;
-    private String pass;
-}

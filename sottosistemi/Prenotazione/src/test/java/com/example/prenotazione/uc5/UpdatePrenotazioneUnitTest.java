1cb05dff25c32f59f4d049b1202e953071ef24bc
AlessandroClericuzio <alessandroclericuzio22@gmail.com>
2023-02-10 11:55:33 +0100

    Modifica e aggiunto commenti  in UpdatePrenotazioneUnitTest

--- sottosistemi/Prenotazione/src/test/java/com/example/prenotazione/uc5/UpdatePrenotazioneUnitTest.java
+++ sottosistemi/Prenotazione/src/test/java/com/example/prenotazione/uc5/UpdatePrenotazioneUnitTest.java
@@ -1,10 +1,9 @@
 package com.example.prenotazione.uc5;
-
 import com.example.prenotazione.exception.PrenotazioneNotFoundException;
 import com.example.prenotazione.model.Prenotazione;
 import com.example.prenotazione.repository.PrenotazioneRepository;
 import com.example.prenotazione.service.PrenotazioneService;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -12,9 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class UpdatePrenotazioneUnitTest {
   @Autowired
@@ -23,7 +22,7 @@ class UpdatePrenotazioneUnitTest {
   PrenotazioneRepository repository;
   SimpleDateFormat smp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
 
-  @Before
+  @BeforeEach
   void setUp() {
     repository = Mockito.mock(PrenotazioneRepository.class);
     service = new PrenotazioneService(repository);
@@ -32,13 +31,16 @@ class UpdatePrenotazioneUnitTest {
   @Test
   void checkUpadatePrenotazioneCodiceNotFound() throws ParseException {
     //prenotazione da aggiornare con codice errato
-    Prenotazione prenotazioneExpected = new Prenotazione("63e107448ed417204f3064de", "RSSMRC75R13F839Q", smp.parse("2023-05-25T15:30"), "4", "2", false);
+    String codicePrenotazione = "63e107448ed417204f3064de";
+    Prenotazione prenotazioneExpected = new Prenotazione(codicePrenotazione, "RSSMRC75R13F839Q", smp.parse("2023-05-25T15:30"), "4", "2", false);
+
+    Mockito.when(repository.findById(codicePrenotazione)).thenReturn(Optional.empty());
 
     //verifica se il metodo lancia un eccezione per il codice errato
     assertThrows(
         PrenotazioneNotFoundException.class,
-        () -> service.updatePrenotazione(prenotazioneExpected.getCodice(), prenotazioneExpected),
-        "Prenotazione con CODICE: |" + prenotazioneExpected.getCodice() + "| non trovata"
+        () -> service.updatePrenotazione(codicePrenotazione, prenotazioneExpected),
+        "Prenotazione con CODICE: |" + codicePrenotazione + "| non trovata"
     );
   }
 
@@ -46,12 +48,15 @@ class UpdatePrenotazioneUnitTest {
   @Test
   void checkUpadatePrenotazione() throws ParseException {
     //prenotazione da aggiornare
-    Prenotazione prenotazioneExpected = new Prenotazione("63e107448ed417204f3064de", "RSSMRC75R13F839Q", smp.parse("2023-05-25T15:30"), "4","2", false );
+    String codicePrenotazione = "63e107448ed417204f3064de";
+    Prenotazione prenotazioneExpected = new Prenotazione(codicePrenotazione, "RSSMRC75R13F839Q", smp.parse("2023-05-25T15:30"), "4","2", false );
+
+    Mockito.when(repository.findById(codicePrenotazione)).thenReturn(Optional.of(prenotazioneExpected));
 
     //aggiornamento della prenotazione nel DB
     service.updatePrenotazione(prenotazioneExpected.getCodice(), prenotazioneExpected);
 
-    //verifico se l'aggiornamento è andato a buon fine
-    assertEquals(service.mapToPrenotazioneResponse(prenotazioneExpected), service.getById("63e107448ed417204f3064de"));
+    //verifico se l'aggiornamento Ã¨ andato a buon fine
+    assertEquals(service.mapToPrenotazioneResponse(prenotazioneExpected), service.getById(codicePrenotazione));
   }
 }

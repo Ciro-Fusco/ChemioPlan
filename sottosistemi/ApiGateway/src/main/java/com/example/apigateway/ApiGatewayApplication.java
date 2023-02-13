28733bb6103cbc6118ea66afa2a691f7461f11f8
francM13 <matteisfrancesco@gmail.com>
2023-02-13 22:03:47 +0100

    aggiunto javadoc ApiGatewayApplication

--- sottosistemi/ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ sottosistemi/ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>La classe ApiGatewayApplication lancia il servizio spring.</p>
+ */
 @SpringBootApplication
 public class ApiGatewayApplication {
   public static void main(String[] args) {

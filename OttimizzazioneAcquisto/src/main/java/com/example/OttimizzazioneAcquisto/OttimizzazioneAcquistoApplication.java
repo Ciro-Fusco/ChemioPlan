4cd11b6cfe66a818e7aa58ec2b00e49be3388bba
francM13 <matteisfrancesco@gmail.com>
2023-02-13 22:18:42 +0100

    check style

--- OttimizzazioneAcquisto/src/main/java/com/example/OttimizzazioneAcquisto/OttimizzazioneAcquistoApplication.java
+++ OttimizzazioneAcquisto/src/main/java/com/example/OttimizzazioneAcquisto/OttimizzazioneAcquistoApplication.java
@@ -3,6 +3,11 @@ package com.example.OttimizzazioneAcquisto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * Lancia il microservizio.
+ * </p>
+ **/
 @SpringBootApplication
 public class OttimizzazioneAcquistoApplication {
 

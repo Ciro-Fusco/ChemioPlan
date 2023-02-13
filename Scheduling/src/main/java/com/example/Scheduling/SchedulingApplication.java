2b083b6aa27c3fb87d652444f97c176fb971608e
francM13 <matteisfrancesco@gmail.com>
2023-02-13 22:15:05 +0100

    rifattorizzato SchedulingApplication

--- Scheduling/src/main/java/com/example/Scheduling/SchedulingApplication.java
+++ Scheduling/src/main/java/com/example/Scheduling/SchedulingApplication.java
@@ -3,11 +3,15 @@ package com.example.Scheduling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>Lancia il microservizio.</p>
+ */
 @SpringBootApplication
 public class SchedulingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchedulingApplication.class, args);
-	}
+  public static void main(String[] args) {
+
+    SpringApplication.run(SchedulingApplication.class, args);
+  }
 
 }

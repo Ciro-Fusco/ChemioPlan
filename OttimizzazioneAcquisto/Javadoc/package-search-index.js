b698819f8d9a52c07357170d14909eb2fae0190c
francM13 <matteisfrancesco@gmail.com>
2023-02-16 08:15:46 +0100

    Generati Javadoc

---
+++ OttimizzazioneAcquisto/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.ottimizzazioneacquisto"},{"l":"com.example.ottimizzazioneacquisto.control"},{"l":"com.example.ottimizzazioneacquisto.service"}];updateSearchResults();
